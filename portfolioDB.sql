2a276ab1576f005c4432e36ba2727e840aebea51
mctrann <mctrann@Gmail.com>
2017-03-24 16:07:36 -0500

    new DB
    updated DB

--- portfolioDB.sql
+++ portfolioDB.sql
@@ -1,13 +1,11 @@
-#version 1.0 of DataTables 
 drop table if exists EmailAddress;
 drop table if exists Address;
 drop table if exists States;
 drop table if exists Countries;
-drop table if exists PortPerson;
-drop table if exists Person;
 drop table if exists PortAsset;
 drop table if exists Asset;
 drop table if exists Portfolio;
+drop table if exists Person;
 
 create table Person ( 
 personID Integer auto_increment not null primary key unique,
@@ -25,8 +23,8 @@ assetCode varchar(20) not null,
 assetName varchar(50) not null,
 assetType varchar(1) not null,
 apr double,
-quartDiv double,
-BRR double,
+quarterlyDividend double,
+baseRateReturn double,
 beta double,
 omega double,
 stockSymbol varchar(50),
@@ -34,6 +32,18 @@ constraint uniqueValues unique (assetCode,assetName),
 totalValue double,
 sharePrice double
 )engine=InnoDB,collate=latin1_general_cs; 
+
+create table Portfolio ( 
+portfolioID Integer not null primary key auto_increment unique,
+managerID integer not null,
+ownerID integer not null,
+beneficiaryID integer,
+foreign key (managerID) references Person(personID),
+foreign key (ownerID) references Person(personID),
+foreign key (beneficiaryID) references Person(personID),
+portCode varchar(20) not null,
+constraint uniqueValues unique (portCode)
+)engine=InnoDB,collate=latin1_general_cs;  
  
 create table Countries (
 countryID integer not null primary key auto_increment unique,
@@ -42,50 +52,29 @@ countryName varchar(200) not null
 
  create table States (
 stateID integer not null primary key auto_increment unique,
-countryID integer not null,
-stateName varchar(200),
-foreign key (countryID) references Countries(countryID)
+stateAbbreviation varchar(3),
+stateName varchar(200)
 )engine=InnoDB,collate=latin1_general_cs; 
 
 create table Address ( 
 addressID integer not null primary key auto_increment unique, 
 personID Integer not null, 
+stateID integer not null, 
+countryID integer not null,
 street varchar(255),
 city varchar(100) , 
-stateID integer not null, 
-zipcode int, 
+zipcode integer, 
 foreign key (personID) references Person(personID),
-foreign key (stateID) references States(stateID)
+foreign key (stateID) references States(stateID),
+foreign key (countryID) references Countries(countryID)
 )engine=InnoDB,collate=latin1_general_cs;  
 
-create table Portfolio ( 
-portfolioID Integer not null primary key auto_increment unique,
-portCode varchar(20) not null,
-totalFee double not null,
-commissions double not null,
-weightedRisk double not null,
-totalReturnVal double not null,
-totalPortVal double not null,
-constraint uniqueValues unique (portCode)
-)engine=InnoDB,collate=latin1_general_cs; 
-
-create table PortPerson (
-portPersonID Integer not null primary key auto_increment unique,
-portfolioID Integer not null,
-managerID integer not null,
-ownerID integer not null,
-beneficiaryID integer,
-foreign key (managerID) references Person(personID),
-foreign key (ownerID) references Person(personID),
-foreign key (beneficiaryID) references Person(personID),
-foreign key (portfolioID) references Portfolio(portfolioID)
-)engine=InnoDB,collate=latin1_general_cs; 
 
 create table PortAsset (
 portAssetID Integer not null primary key auto_increment unique,
 assetID integer not null,
 portfolioID integer not null,
-portAssetVal double not null, #for share price or percent investment or totalval of deposit
+portAssetValue double not null, #for share price or percent investment or totalval of deposit
 foreign key (assetID) references Asset(assetID),
 foreign key (portfolioID) references Portfolio(portfolioID)
 ) engine=InnoDB,collate=latin1_general_cs; 
@@ -96,6 +85,60 @@ emailAddress varchar(100) not null unique,
 personID integer not null,
 foreign key (personID) references Person(personID) 
 )engine=InnoDB,collate=latin1_general_cs; 
+
+insert into States (stateAbbreviation, stateName) value ('AL', 'Alabama');
+insert into States (stateAbbreviation, stateName) value ('AK', 'Alaska');
+insert into States (stateAbbreviation, stateName) value ('AR', 'Arkansas');
+insert into States (stateAbbreviation, stateName) value ('AZ', 'Arizona');
+insert into States (stateAbbreviation, stateName) value ('CA', 'California');
+insert into States (stateAbbreviation, stateName) value ('CO', 'Colorado');
+insert into States (stateAbbreviation, stateName) value ('CT', 'Connecticut');
+insert into States (stateAbbreviation, stateName) value ('DE', 'Delaware');
+insert into States (stateAbbreviation, stateName) value ('DC', 'District of Columbia');
+insert into States (stateAbbreviation, stateName) value ('FL', 'Florida');
+insert into States (stateAbbreviation, stateName) value ('GA', 'Georgia');
+insert into States (stateAbbreviation, stateName) value ('HI', 'Hawaii');
+insert into States (stateAbbreviation, stateName) value ('ID', 'Idaho');
+insert into States (stateAbbreviation, stateName) value ('IL', 'Illinois');
+insert into States (stateAbbreviation, stateName) value ('IN', 'Indiana');
+insert into States (stateAbbreviation, stateName) value ('IA', 'Iowa');
+insert into States (stateAbbreviation, stateName) value ('KS', 'Kansas');
+insert into States (stateAbbreviation, stateName) value ('KY', 'Kentucky');
+insert into States (stateAbbreviation, stateName) value ('LA', 'Lousiana');
+insert into States (stateAbbreviation, stateName) value ('ME', 'Maine');
+insert into States (stateAbbreviation, stateName) value ('MD', 'Maryland');
+insert into States (stateAbbreviation, stateName) value ('MA', 'Massachusetts');
+insert into States (stateAbbreviation, stateName) value ('MI', 'Michigan');
+insert into States (stateAbbreviation, stateName) value ('MN', 'Minnesota');
+insert into States (stateAbbreviation, stateName) value ('MS', 'Mississippi');
+insert into States (stateAbbreviation, stateName) value ('MO', 'Missouri');
+insert into States (stateAbbreviation, stateName) value ('MT', 'Montana');
+insert into States (stateAbbreviation, stateName) value ('NE', 'Nebraska');
+insert into States (stateAbbreviation, stateName) value ('NV', 'Nevada');
+insert into States (stateAbbreviation, stateName) value ('NH', 'New Hampshire');
+insert into States (stateAbbreviation, stateName) value ('NJ', 'New Jersey');
+insert into States (stateAbbreviation, stateName) value ('NM', 'New Mexico');
+insert into States (stateAbbreviation, stateName) value ('NY', 'New York');
+insert into States (stateAbbreviation, stateName) value ('NC', 'North Carolina');
+insert into States (stateAbbreviation, stateName) value ('ND', 'North Dakota');
+insert into States (stateAbbreviation, stateName) value ('OH', 'Ohio');
+insert into States (stateAbbreviation, stateName) value ('OK', 'Oklahoma');
+insert into States (stateAbbreviation, stateName) value ('OR', 'Oregon');
+insert into States (stateAbbreviation, stateName) value ('PA', 'Pennsylvania');
+insert into States (stateAbbreviation, stateName) value ('RI', 'Rohde Island');
+insert into States (stateAbbreviation, stateName) value ('SC', 'South Carolina');
+insert into States (stateAbbreviation, stateName) value ('SD', 'South Dakota');
+insert into States (stateAbbreviation, stateName) value ('TN', 'Tennessee');
+insert into States (stateAbbreviation, stateName) value ('TX', 'Texas');
+insert into States (stateAbbreviation, stateName) value ('UT', 'Utah');
+insert into States (stateAbbreviation, stateName) value ('VT', 'Vermont');
+insert into States (stateAbbreviation, stateName) value ('VA', 'Virginia');
+insert into States (stateAbbreviation, stateName) value ('WA', 'Washington');
+insert into States (stateAbbreviation, stateName) value ('WV', 'West Virgina');
+insert into States (stateAbbreviation, stateName) value ('WI', 'Wisconsin');
+insert into States (stateAbbreviation, stateName) value ('WY', 'Wyoming');
+
+
 
 
 
